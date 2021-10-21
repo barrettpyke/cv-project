55ac956b84ab20dbec52a3b50a0fecb6a7bb5dd1
Barrett <barrettpyke@gmail.com>
2021-10-21 00:28:59 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ render() {
         <General />
         <Education />
         <Experience />
+        <div id="footer">
+          <div id="footerText">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        </div>
     </div>
   );
 }
