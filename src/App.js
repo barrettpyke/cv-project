d083760fcf89bab2396c5a06a1974a86e5a8e1c9
Barrett <barrettpyke@gmail.com>
2021-10-22 08:46:48 -0500

    removed print and footer

--- src/App.js
+++ src/App.js
@@ -14,14 +14,10 @@ render() {
     <div id="container">
       <div id="header">
         <h1 id="heading">CV Generator</h1>
-        <button type="button" id="printButton" className="btn btn-success">Print to PDF</button>
       </div>
         <General />
         <Education />
         <Experience />
-        <div id="footer">
-          <div id="footerText">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-        </div>
     </div>
   );
 }
